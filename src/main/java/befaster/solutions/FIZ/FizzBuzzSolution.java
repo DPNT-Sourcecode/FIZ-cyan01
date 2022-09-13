22665217e0526ee440a09214cc9614ec034b4781
sbx_user1051 <sbx_user1051@169.254.155.77>
2022-09-13 18:43:23 +0000

    Tue Sep 13 18:43:23 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -9,6 +9,10 @@ public class FizzBuzzSolution {
        String fizz = "", buzz = "";
        Integer num = number;
        if(number % 5 == 0 && number % 3 == 0) return "fizz buzz";
+       if(number % 5 == 0)
+           buzz = "buzz";
+       if(number % 3 == 0)
+           fizz = "fizz";
        while(number > 0){
            if(number % 10 == 3)
                fizz = "fizz";
@@ -30,4 +34,5 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
